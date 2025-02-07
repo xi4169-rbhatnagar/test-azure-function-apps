9e1b946959491d0a0709bdc8095c49dc7cdcf552
Rishabh Bhatnagar <rishabh.bhatnagar@xebia.com>
2025-02-07 15:01:21 +0530

    Use a WSGI app for flask
    Signed-off-by: Rishabh Bhatnagar <rishabh.bhatnagar@xebia.com>

--- function_app.py
+++ function_app.py
@@ -2,4 +2,4 @@ import azure.functions as func
 
 from server import app
 
-func_app = func.AsgiFunctionApp(app=app, http_auth_level=func.AuthLevel.ANONYMOUS)
+func_app = func.WsgiFunctionApp(app=app, http_auth_level=func.AuthLevel.ANONYMOUS)
